f26cd592e1d058ea714f0aea8ae9153ae4586d7a
ElderGr <gbielrs@gmail.com>
2021-08-10 16:02:36 -0300

    feat: code review

--- src/services/categoryService.ts
+++
@@ -1,9 +0,0 @@
-import { getRepository } from "typeorm";
-import Categories from "../entities/Categories";
-
-export async function getCategories(){
-    
-    const categories = await getRepository(Categories).find();
-    
-    return categories;
-}
